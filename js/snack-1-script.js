2aadfe4dd350c8a8b80c6c6696be7f70b4c89e58
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-25 15:34:33 +0100

    Create odd user number

---
+++ js/snack-1-script.js
@@ -0,0 +1,14 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all'utente di inserire un numero,
+// se è dispari inseriscilo nell'array.
+
+const numberList = []
+
+
+for(let i=0; i < 6; i++){
+    const userNumber = parseInt(prompt(`Inserisci un numero`))
+    
+    if(userNumber % 2 == 1){
+        console.log(userNumber)
+    }
+}
