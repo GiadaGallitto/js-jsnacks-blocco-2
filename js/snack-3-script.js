55b90872908609efa609ccc68358d4a16326ab0c
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-25 16:50:41 +0100

    Add average calculator

--- js/snack-3-script.js
+++ js/snack-3-script.js
@@ -1,8 +1,10 @@
-// Calcola la somma dei primi 10 numeri di un array.
+// Calcola la somma e la media dei primi 10 numeri di un array.
 
 const numbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 let somma = 0;
+
+let average;
 
 // Creo un ciclo in cui un elemento continua a sommarsi fino al decimo elemento della lista
 for(let i=0; i < 10; i++){
@@ -11,6 +13,10 @@ for(let i=0; i < 10; i++){
     // fase del ciclo), si aggiunge ad essa, sommandosi insieme agli altri, dandomi il risultato della somma totale
     somma+= numbers[i];
     // Il += in forma estesa vuol dire somma = somma + numbers[i]
+
+    average = parseInt(somma / numbers.length) || 0;
 }
 
-console.log(somma)
+console.log(somma, average)
+
+
