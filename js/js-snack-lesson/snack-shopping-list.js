deb3f8f86352a4882435409c3ae4f8a2301b9856
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-28 15:30:40 +0100

    Create li elements

---
+++ js/js-snack-lesson/snack-shopping-list.js
@@ -0,0 +1,27 @@
+let shoppingList = [
+    "Cera al Lanciafiamme",
+    "Sciarpa per piccioni",
+    "Calice di fuoco",
+    "Maschera di IronMan",
+    "Bentley infuocata",
+    "Sacco a pelo per gatti",
+    "Piume di pavone"
+];
+
+const ulElement = document.querySelector("ul.list-group");
+
+let i ;
+
+while (i < shoppingList.length){
+    const newLiElement = document.createElement("li");
+    
+    newLiElement.classList.add("list-group-item");
+    
+    newLiElement.append(shoppingList[i]);
+    newLiElement.innerHTML = shoppingList[i];
+    
+    ulElement.append(newLiElement);
+
+    console.log(newLiElement)
+}
+
