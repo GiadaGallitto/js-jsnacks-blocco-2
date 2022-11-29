66754573146116e8b36cbbe4e46312a0c35a6ffe
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-29 10:09:30 +0100

    Add comments

--- js/js-snack-lesson/snack-shopping-list.js
+++ js/js-snack-lesson/snack-shopping-list.js
@@ -9,21 +9,28 @@ let shoppingList = [
 ];
 
 const ulElement = document.querySelector("ul.list-group");
-
+// Seleziono l'elemento ul presente in HTML
 let i = 0;
 
 while (i < shoppingList.length){
     const newLiElement = document.createElement("li");
+    // Creo in Js, un elemento li (che si ripeterà per tutti gli elementi della lista, grazie a while)
     
     newLiElement.classList.add("list-group-item");
+    // Aggiungo ad ogni li, una classeù (in sto caso, tipica di bootstrap)
     
     newLiElement.append(shoppingList[i]);
-    newLiElement.innerHTML = shoppingList[i];
+    // Ad ogni li appendo un elemento della lista
+
+    // newLiElement.innerHTML = shoppingList[i];
+    // Posso utilizzare anche questo metodo al posto di append
     
     ulElement.append(newLiElement);
+    // Così appendo ogni li già composto, all'interno dell'ul
 
     console.log(newLiElement);
 
     i++;
+    // Scrivo le istruzioni di uscita per far terminare il loop alla fine delle indicazioni di esecuzione
 }
 
